5244f1ab2adbd13aa246806aed2f7bb01e5f2577
ecabigting <ericcabigting@outlook.com>
2024-02-15 22:11:12 +0400

    - added backbutton re-usable component

--- auth-v5-example/components/auth/card-wrapper.tsx
+++ auth-v5-example/components/auth/card-wrapper.tsx
@@ -1,8 +1,9 @@
 "use client";
 
 import { Card, CardContent, CardFooter, CardHeader } from "@/components/ui/card";
-import { Header } from "./header";
-import { Social } from "./social";
+import { Header } from "@/components/auth//header";
+import { Social } from "@/components/auth//social";
+import { BackButton } from "@/components/auth/back-button";
 
 interface CardWrapperProps {
 	children: React.ReactNode;
@@ -30,6 +31,9 @@ export const CardWrapper = ({
 					<Social />
 				</CardFooter>
 			)}
+			<CardFooter>
+				<BackButton label={backButtonLabel} href={backButtongHref} />
+			</CardFooter>
 		</Card>
 	);
 };
