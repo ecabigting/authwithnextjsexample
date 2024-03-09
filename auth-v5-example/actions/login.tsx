170475dd4d5818d51df677dc59e0baededec36de
ecabigting <ericcabigting@outlook.com>
2024-03-09 21:21:58 +0400

    - added the resend package to send verification email - updated login.ts in action to implement sending verification email when user tries to login with unverified email - updated register.ts in action to implement sending verification email when user registers - implement the verification email functionality

--- auth-v5-example/actions/login.tsx
+++ auth-v5-example/actions/login.tsx
@@ -6,6 +6,7 @@ import { DEFAULT_LOGIN_REDIRECT } from "@/routes";
 import { AuthError } from "next-auth";
 import { getUserByEmail } from "@/data/user";
 import { generateVerificationToken } from "@/lib/tokens";
+import { sendVerificationEmail } from "@/lib/mail";
 
 export const login = async (values: z.infer<typeof LoginSchema>) => {
 	const validatedFields = LoginSchema.safeParse(values);
@@ -21,7 +22,7 @@ export const login = async (values: z.infer<typeof LoginSchema>) => {
 
 	if (!existingUser.emailVerified) {
 		const verificationToken = await generateVerificationToken(existingUser.email);
-		console.log("verifying email... confirmation email sent");
+		await sendVerificationEmail(verificationToken.email, verificationToken.token);
 		return { success: "Confirmation Email Sent!" };
 	}
 
