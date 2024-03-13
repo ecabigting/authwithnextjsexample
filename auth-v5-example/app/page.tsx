41aecbfe324b2797d8c88edd4f24687feff50595
ecabigting <ericcabigting@outlook.com>
2024-03-13 18:04:39 +0400

    Added new User Info in server page

--- auth-v5-example/app/page.tsx
+++ auth-v5-example/app/page.tsx
@@ -10,7 +10,7 @@ const font = Poppins({
 
 export default function Home() {
 	return (
-		<main className='flex h-full flex-col items-center justify-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-400 to-blue-800'>
+		<main className='flex h-full flex-col items-center justify-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-gray-400 to-gray-800'>
 			<div className='space-y-6 text-center'>
 				<h1 className={cn("text-6xl font-semibold text-white drop-shadow-md", font.className)}>Auth</h1>
 				<p className='text-white text-lg'>Auth Service using Next Auth V5</p>
--- auth-v5-example/components/auth/login-form.tsx
+++ auth-v5-example/components/auth/login-form.tsx
@@ -14,6 +14,7 @@ import { login } from "@/actions/login";
 import { LoadingSpinner } from "../ui/spinner";
 import { useSearchParams } from "next/navigation";
 import Link from "next/link";
+import { logout } from "@/actions/logout";
 
 export const LoginForm = () => {
 	const searchParams = useSearchParams();
